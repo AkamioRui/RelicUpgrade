3c20d8d0d930aa1010b29d62203f7e1821395374
akamio <wiboworuimakaio@gmail.com>
2026-01-02 16:46:19 +0700

    express server 1.0

--- express.js
+++ express.js
@@ -1,40 +1,61 @@
 const express = require('express');
 const path = require('node:path');
 const { exec, execFile } = require('node:child_process');
+const fs =  require('node:fs')
 
-function asyncExec(program,args){
+
+function asyncExecFile(program,args){
     return new Promise((resolve,reject)=>{
-
+        execFile(program,args,(err,stdout,stderr)=>{
+            if(err) reject({err,stderr,stdout})
+            else resolve({stderr,stdout})
+        })
     })
 }
 
 //------------------------app----------------------
 
 const app = express();
+app.use((req,res,next)=>{
+    console.log('@@@@@@'+req.url);
+    next();
+})
 app.use(express.static(path.join(__dirname,'UI')))
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'UI/relicUpgradeGUI.html'))
 })
-app.get('/calculate/:piece/:mainstat/_/*substat',(req,res)=>{
+app.get('/calculate/:piece/:mainstat/_/*substat', async(req,res)=>{
     const {piece,mainstat,substat}= req.params;
     
     const cProgram = path.join(__dirname,'cFile/relicUpgrade.exe');
     const args = ['destination', piece,mainstat,'threshold',...substat];
 
-    //make it a promise
+    res.header('Content-Type','application/json')
+    res.write('[')
     for(let threshold = 1; threshold<=9; threshold++){
         args[0] = path.join(__dirname,'result',
             threshold+'.json')
         args[3] = threshold;
-        
-        execFile(cProgram,args,(err,stdout,stderr)=>{
-            console.log({err,stdout,stderr});
-        })
-    }
 
+        // await asyncExecFile(cProgram,args)
+
+        await new Promise((done,err)=>{
+            fs.createReadStream(args[0])
+                .on('end',done)
+                .on('error',err)
+                .pipe(res,{end:false})
+        })
+        
+        
+        if(threshold<9) res.write(',')
+        
+    }
+    res.write(']')
     
     
-    res.end(JSON.stringify(args));
+    
+    
+    res.end();
 })
 
 //---------------------------------server
